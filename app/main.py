90d0a41ee470ebc7a2be041f4f3b76843322f2f7
0xffvirus <0xffvirus@gmail.com>
2026-05-12 22:06:58 +0300

    fixed main.py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 import traceback
 import logging
+import os
 
 scheduler = BackgroundScheduler()
 
